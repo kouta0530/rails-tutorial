98a83665c6ea8e06d2416836b7b69c74b374868c
kouta0530 <16jk047@ymklab.net>
2021-05-21 00:43:46 +0000

    fix(Login): logoutパスのurlを#destoryに修正

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
-  post '/logout', to: 'session#destory' 
+  delete '/logout', to: 'session#destroy' 
   resources :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
