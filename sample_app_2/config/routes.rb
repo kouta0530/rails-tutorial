639624674983e35036a24a10c392c12ac88251e6
kouta0530 <16jk047@ymklab.net>
2021-08-11 16:47:03 +0900

    fix(routes): userページへのパスをsign_pathに変更

--- sample_app_2/config/routes.rb
+++ sample_app_2/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#home'
 
   get '/help', to: 'static_pages#help'
@@ -9,5 +7,6 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
