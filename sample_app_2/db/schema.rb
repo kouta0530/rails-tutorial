7c64811ee87ed35bcb2e29be16d6f0f1630f099a
kouta0530 <16jk047@ymklab.net>
2021-08-12 21:31:52 +0900

    fix(db): コード規約ようのコードにリファクタリング

--- sample_app_2/db/schema.rb
+++ sample_app_2/db/schema.rb
@@ -10,13 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210812121041) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_210_812_121_041) do
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
