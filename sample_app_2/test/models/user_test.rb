cfa94d7f6d2f811d696ccda26a8aa0eeacafb836
kouta0530 <16jk047@ymklab.net>
2021-08-16 09:39:40 +0900

    feat(test): password属性が空でないか確認するテストの追加

--- sample_app_2/test/models/user_test.rb
+++ sample_app_2/test/models/user_test.rb
@@ -56,4 +56,9 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
+    assert_not @user.valid?
+  end
 end
