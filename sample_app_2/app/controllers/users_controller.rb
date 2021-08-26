179566ee7710221d9902a9a26aaa74a35730b8d2
kouta0530 <16jk047@ymklab.net>
2021-08-26 23:53:04 +0900

    feat(controller): サインアップフォーム投稿時のメソッドを作成

--- sample_app_2/app/controllers/users_controller.rb
+++ sample_app_2/app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create 
+    @user = User.new(params[:user])
+    if @user.save
+      
+    else
+      render 'new'
+    end
 end
